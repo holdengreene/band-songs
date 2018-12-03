3fc2287b2fd6799c6f7078eca954292859431f45
Holden Greene <hgreene@cmnhospitals.org>
2018-12-02 22:18:02 -0700

    Loop through songs

---
+++ components/SongCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <h3 class="title">{{ song.title }}</h3>
+      <p class="subtitle">{{ song.chords }}</p>
+    </div>
+    <footer class="card-footer">
+      <p class="card-footer-item">
+        View Song
+      </p>
+      <p class="card-footer-item">
+        Edit Song
+      </p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    song: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+  color: $dark-heading;
+}
+
+.subtitle {
+  font-weight: 600;
+}
+
+.card-footer-item {
+  margin: 0;
+}
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,90 @@
 <template>
-  <section class="container">
-    <div>
-      <h1 class="title">band-songs</h1>
-      <h2 class="subtitle">Track all the songs</h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
+  <div>
+    <div class="useful-text">
+      <h1>Band Songs</h1>
+      <h2>Track The Songs</h2>
+      <p>Stop forgetting what songs you know.</p>
     </div>
-  </section>
+
+    <div class="song-grid">
+      <SongCard v-for="song in songs" :key="song.id" :song="song"/>
+    </div>
+  </div>
 </template>
+
+<script>
+import SongCard from '~/components/SongCard.vue'
+
+export default {
+  components: {
+    SongCard
+  },
+  data() {
+    return {
+      songs: [
+        {
+          title: 'Test Song',
+          chords: 'Am C G'
+        },
+        {
+          title: 'A different Song',
+          chords: 'Am Em'
+        },
+        {
+          title: "Let's Not",
+          chords: 'Am Em C G F Am'
+        },
+        {
+          title: 'Hit Me Again',
+          chords: 'E G#m A B'
+        },
+        {
+          title: 'True Truant',
+          chords: 'Gb Cb Abm Ebm'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.song-grid {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+
+  .card {
+    margin: rem(5px);
+    width: calc(25% - #{rem(10px)});
+  }
+}
+
+@supports (display: grid) {
+  .song-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(rem(270px), 1fr));
+    grid-gap: rem(10px);
+
+    .card {
+      margin: 0;
+      width: 100%;
+    }
+  }
+}
+
+@media screen and (max-width: rem(1023px)) {
+  .song-grid .card {
+    width: calc(50% - #{rem(10px)});
+  }
+
+  @supports (display: grid) {
+    .song-grid {
+      .card {
+        width: 100%;
+      }
+    }
+  }
+}
+</style>
 
