c10d226338c5c58121b73c386eb1beea928b3eaa
Holden Greene <hgreene@cmnhospitals.org>
2018-12-21 16:50:22 -0700

    Vuex store is now modular

--- store/index.js
+++ store/index.js
@@ -1,25 +1,29 @@
 import Vuex from 'vuex';
 import axios from 'axios';
 
-const createStore = () => {
-  return new Vuex.Store({
-    state: () => ({
-      songsList: {}
-    }),
-    mutations: {
-      setSongs(state, songs) {
-        state.songsList = { ...state.songsList, songs };
-      }
-    },
-    actions: {
-      async nuxtServerInit({ commit, state }) {
-        const allSongs = `http://localhost:8080/bands/1/songs`;
-        const { data } = await axios.get(allSongs);
+export const state = () => ({
+  songsList: {},
+  songsEmpty: false
+});
 
-        commit('setSongs', data.songs);
-      }
-    }
-  });
+export const mutations = {
+  setSongs(state, songs) {
+    state.songsList = { ...state.songsList, songs };
+  },
+  setSongsEmpty(state, empty) {
+    state.songsEmpty = empty;
+  }
 };
 
-export default createStore;
+export const actions = {
+  async nuxtServerInit({ commit, state }) {
+    try {
+      const allSongs = `http://localhost:8080/bands/1/songs`;
+      const { data } = await axios.get(allSongs);
+
+      commit('setSongs', data.songs);
+    } catch (e) {
+      commit('setSongsEmpty', true);
+    }
+  }
+};
