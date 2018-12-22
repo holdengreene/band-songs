e99fead8fa86d84ac60e23361bd842689c04a711
Holden Greene <hgreene@cmnhospitals.org>
2018-12-22 14:29:26 -0700

    Save current song in data

--- pages/_song.vue
+++ pages/_song.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="song-wrap">
     <!-- <h1>{{ getSong.title }}</h1> -->
-    {{ getCurrSong }}
+    {{ currSong }}
     <nuxt-link to="168">Hello</nuxt-link>
   </div>
 </template>
 
 <script>
 export default {
+  // Add current song to data on page load
+  async asyncData({ params, store }) {
+    const songId = parseInt(params.song);
+
+    const currSong = store.getters.getSongById(songId);
+
+    return { currSong };
+  },
+  // Get the current song from vuex at any time
   computed: {
-    getCurrSong: function() {
+    getSong: function() {
       // Have to convert the id to a number
       const songId = parseInt(this.$route.params.song);
       return this.$store.getters.getSongById(songId);
